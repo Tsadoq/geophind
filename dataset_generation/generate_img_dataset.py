76aca93e0b4fb867eef9841570c74c0672d18db1
tsadoq <github.v5331@passmail.net>
2024-07-16 21:49:08 +0200

    save max 1000 points per country

--- dataset_generation/generate_img_dataset.py
+++ dataset_generation/generate_img_dataset.py
@@ -7,23 +7,27 @@ from image_scraper import get_image_from_area
 
 
 def generate_img_dataset(file_path:str):
+    country_counter = {}
     tot_tiles = 49_000
     df = pd.read_pickle(file_path)
     for i in range(0, len(df)):
+        lat = df.iloc[i]['point'].y
+        lon = df.iloc[i]['point'].x
+        country = df.iloc[i]['geounit']
+        if country_counter.get(country, 1000) <= 0:
+            continue
         if i % 10 == 0:
             print(f'Processing {i} of {len(df)}')
         if i % 1000 == 0:
             print('Waiting 10 minutes')
             sleep(600)
-        lat = df.iloc[i]['point'].y
-        lon = df.iloc[i]['point'].x
-        country = df.iloc[i]['geounit']
         points, n_tiles = get_image_from_area(
             lat=lat,
             lon=lon,
             country=country,
             max_distance_meters=100_000
         )
+        country_counter[country] = country_counter.get(country, 1000) - len(points)
         if i % 10 == 0:
             print(f'You still have {tot_tiles} before sleeping for one day')
         tot_tiles -= n_tiles
